1dad330c59988721c5681df3059ba622acee37d5
Your Name <you@example.com>
2016-01-01 22:00:30 +0000

    change port listening to dynamic

--- app.js
+++ app.js
@@ -38,4 +38,6 @@ app.post("/add_high_score_entry", function(req, res){
 	});
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
